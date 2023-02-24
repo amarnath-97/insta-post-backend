2965dc9661a6181924ce54f5c2a29579ba1fa9ab
F22 Labs <amarnath@f22labs.com>
2023-02-24 17:28:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+
+app.use('/', async (req, res) => {
+    const {code} = req.query;
+    const response = await fetch(`https://api.instagram.com/oauth/access_token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `client_id=708865567383723&client_secret=5b8d7e24b6e6ba4746717f3f47900f69&grant_type=authorization_code&redirect_uri=https://insta-post.vercel.app/&code=${code}`
+    });
+    const data = await response.json();
+
+    res.send(data)
+});
+
+
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>{
+    console.log('Server is up and running on port:'+ port);
+});
